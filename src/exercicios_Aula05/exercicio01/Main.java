b754f382bd4650887d39a87a84d29cf1ef3c3744
xguimiranda <saguilherme16@gmail.com>
2026-03-10 11:36:19 -0300

    FEAT: static bilhete

--- src/exercicios_Aula05/exercicio01/Main.java
+++ src/exercicios_Aula05/exercicio01/Main.java
@@ -4,6 +4,24 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static Bilhete bilhete;
+
+    static {
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Você é Estudante/Professor ou Normal: ");
+        String tipoTarifa = sc.next();
+        while (!tipoTarifa.equalsIgnoreCase("Estudante")
+                && !tipoTarifa.equalsIgnoreCase("Professor")
+                && !tipoTarifa.equalsIgnoreCase("Normal")){
+
+            System.out.print("Erro! Escolha entre Estudante, Professor ou Normal: ");
+            tipoTarifa = sc.next();
+        }
+        bilhete = new Bilhete(nome, tipoTarifa);
+    }
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
