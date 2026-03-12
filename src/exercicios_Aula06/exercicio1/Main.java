2d0af2a38fca4fe36ba2feac4d160b923437f8a4
xguimiranda <saguilherme16@gmail.com>
2026-03-12 09:21:20 -0300

    FEAT: initial main

--- src/exercicios_Aula06/exercicio1/Main.java
+++ src/exercicios_Aula06/exercicio1/Main.java
@@ -2,6 +2,18 @@ package exercicios_Aula06.exercicio1;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MonitorEnergia monitor = new MonitorEnergia(10);
+
+        monitor.registroEnergiaPorDia(2);
+        monitor.registroEnergiaPorDia(2);
+        monitor.registroEnergiaPorDia(4);
+
+        System.out.printf("%.2f",monitor.calcularMedia());
+        System.out.printf("\n%.2f",monitor.calcularMedia(1, 2));
+
+
+
 
     }
 }
