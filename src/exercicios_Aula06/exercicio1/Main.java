5856019eccee6ad5aaca665c0e61396d202439a2
xguimiranda <saguilherme16@gmail.com>
2026-03-12 09:43:31 -0300

    FEAT: new method

--- src/exercicios_Aula06/exercicio1/Main.java
+++ src/exercicios_Aula06/exercicio1/Main.java
@@ -7,9 +7,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         MonitorEnergia monitor = new MonitorEnergia(10);
 
-        monitor.registroEnergiaPorDia(2);
-        monitor.registroEnergiaPorDia(2);
-        monitor.registroEnergiaPorDia(4);
+        do {
+            System.out.printf("Digite a valor do dia %d: ", dia);
+            dia = sc.nextInt();
+            monitor.registroEnergiaPorDia(dia);
+        } while (dia != 0);
 
         System.out.printf("%.2f",monitor.calcularMedia());
         System.out.printf("\n%.2f",monitor.calcularMedia(1, 2));
