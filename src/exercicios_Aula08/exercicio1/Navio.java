dd8863a1cd580f95b0f5713d10954c2d5e3ff8ca
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:54:28 -0300

    FEAT: navio methods

--- src/exercicios_Aula08/exercicio1/Navio.java
+++ src/exercicios_Aula08/exercicio1/Navio.java
@@ -10,3 +10,24 @@ public class Navio {
         this.index = 0;
         this.carga = new Carga[20];
     }
+
+    public boolean permitidoReservar(double peso) {
+        double total = 0;
+        for(int i = 0; i < index; i++) {
+            total += carga[i].getPeso();
+        }
+
+        return (total + peso) <= capacidade;
+    }
+
+    public boolean reservar(Carga carga) {
+        if(index < this.carga.length) {
+            if(permitidoReservar(carga.getPeso())) {
+                this.carga[index] = carga;
+                index++;
+                return true;
+            }
+        }
+        return false;
+    }
+}
