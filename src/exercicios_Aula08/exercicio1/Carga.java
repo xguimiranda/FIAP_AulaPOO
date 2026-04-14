8bbc6be7022cb998ae96f353723f00a90b01f2cb
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:55:38 -0300

    FEAT: carga Methods

--- src/exercicios_Aula08/exercicio1/Carga.java
+++ src/exercicios_Aula08/exercicio1/Carga.java
@@ -15,3 +15,36 @@ public class Carga {
         this.id = random.nextInt(1000, 9999);
         this.peso = random.nextDouble(1000, 5000);
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
