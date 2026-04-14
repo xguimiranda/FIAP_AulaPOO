fc683dbc8332d61c0f69894b19c5382571982838
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:54:41 -0300

    FEAT: imports

--- src/exercicios_Aula08/exercicio1/Util.java
+++ src/exercicios_Aula08/exercicio1/Util.java
@@ -1,4 +1,7 @@
 package exercicios_Aula08.exercicio1;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
 
 public class Util {
 }
