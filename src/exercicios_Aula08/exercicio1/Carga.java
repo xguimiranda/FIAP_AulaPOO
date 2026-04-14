47a8e482f11030c10370f6654eda3380d262bebd
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:55:29 -0300

    FEAT: carga class

--- src/exercicios_Aula08/exercicio1/Carga.java
+++ src/exercicios_Aula08/exercicio1/Carga.java
@@ -3,4 +3,15 @@ package exercicios_Aula08.exercicio1;
 import java.util.Random;
 
 public class Carga {
-}
+    private int id;
+    private String destino;
+    private double peso;
+    private Cliente cliente;
+
+    public Carga(String destino, Cliente cliente) {
+        Random random = new Random();
+        this.destino = destino;
+        this.cliente = cliente;
+        this.id = random.nextInt(1000, 9999);
+        this.peso = random.nextDouble(1000, 5000);
+    }
