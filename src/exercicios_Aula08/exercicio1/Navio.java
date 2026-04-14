b3601362896fac262c1a768c1c436f7735fbbaf4
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:53:55 -0300

    Feat: navio class

--- src/exercicios_Aula08/exercicio1/Navio.java
+++ src/exercicios_Aula08/exercicio1/Navio.java
@@ -1,4 +1,12 @@
 package exercicios_Aula08.exercicio1;
 
 public class Navio {
-}
+    private double capacidade;
+    private int index;
+    private Carga[] carga;
+
+    public Navio() {
+        this.capacidade = 10000;
+        this.index = 0;
+        this.carga = new Carga[20];
+    }
