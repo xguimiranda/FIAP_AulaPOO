e33dce0013dfa5d12ab0d0482e96522a1c41dca5
xguimiranda <saguilherme16@gmail.com>
2026-04-14 10:55:51 -0300

    FEAT: Client class

--- src/exercicios_Aula08/exercicio1/Cliente.java
+++ src/exercicios_Aula08/exercicio1/Cliente.java
@@ -1,4 +1,19 @@
 package exercicios_Aula08.exercicio1;
 
 public class Cliente {
-}
+    private int cnpj;
+    private String cliente;
+
+    public Cliente(int cnpj, String cliente) {
+        this.cnpj = cnpj;
+        this.cliente = cliente;
+    }
+
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
