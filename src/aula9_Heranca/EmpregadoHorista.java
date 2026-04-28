a373818db2bfb67255f2312c9ac75195ad0dd6ae
Laboratório FIAP <saguilherme16@gmail.com>
2026-04-28 10:50:55 -0300

    empregado Horista class

--- src/aula9_Heranca/EmpregadoHorista.java
+++ src/aula9_Heranca/EmpregadoHorista.java
@@ -1,4 +1,16 @@
 package aula9_Heranca;
 
-public class EmpregadoHorista {
+public class EmpregadoHorista extends Empregado {
+    int totalDeHorasTrabalhadas;
+    double valorDaHora;
+
+    public EmpregadoHorista(long matricula, String nome, int totalDeHorasTrabalhadas, double valorDaHora) {
+        super(matricula, nome);
+        this.totalDeHorasTrabalhadas = totalDeHorasTrabalhadas;
+        this.valorDaHora = valorDaHora;
+    }
+
+    public double calcularSalario() {
+        return totalDeHorasTrabalhadas * valorDaHora;
+    }
 }
