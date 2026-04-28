867874c88154208033f005351e5c6c50e2673c78
Laboratório FIAP <saguilherme16@gmail.com>
2026-04-28 10:50:14 -0300

    Empregado Comissionado class

--- src/aula9_Heranca/EmpregadoComissionado.java
+++ src/aula9_Heranca/EmpregadoComissionado.java
@@ -1,4 +1,17 @@
 package aula9_Heranca;
 
-public class EmpregadoComissionado {
+public class EmpregadoComissionado extends Empregado {
+    double totalDeVendas;
+    double comissao;
+
+    public EmpregadoComissionado(long matricula, String nome, double totalDeVendas, double comissao) {
+        super(matricula, nome);
+        this.totalDeVendas = totalDeVendas;
+        this.comissao = comissao;
+    }
+
+    public double calcularSalario() {
+        return totalDeVendas * comissao / 100;
+    }
+
 }
