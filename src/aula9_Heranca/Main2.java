f1556df2c7641418553b8ae6906aed649b1ef085
xguimiranda <saguilherme16@gmail.com>
2026-04-28 11:31:54 -0300

    FEAT: Main2 methods

--- src/aula9_Heranca/Main2.java
+++ src/aula9_Heranca/Main2.java
@@ -3,4 +3,23 @@ package aula9_Heranca;
 import java.util.ArrayList;
 
 public class Main2 {
+    public static void main(String[] args) {
+        ArrayList<Empregado> lista = new ArrayList<>();
+
+        lista.add(new EmpregadoComissionado(1, "Tigresa VIP", 10, 10));
+        lista.add(new EmpregadoHorista(2, "Neymar", 10, 10));
+        lista.add(0, new EmpregadoHorista(2, "Bruna Biancard", 10, 10));
+
+        // for tradicional
+        for (int i = 0; i < lista.size(); i++){
+            System.out.println(lista.get(i).nome);
+        }
+
+        //for generico
+        for (Empregado empregado : lista){
+            System.out.println(empregado);
+            System.out.println(empregado.calcularSalario());
+        }
+
+    }
 }
