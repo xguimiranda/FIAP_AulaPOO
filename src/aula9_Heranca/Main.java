77e7df3cfcf2be273fe36bb8906a3eeb4308a892
Laboratório FIAP <saguilherme16@gmail.com>
2026-04-28 10:51:19 -0300

    Main class

--- src/aula9_Heranca/Main.java
+++ src/aula9_Heranca/Main.java
@@ -1,4 +1,29 @@
 package aula9_Heranca;
 
 public class Main {
+    public static void main(String[] args) {
+        Empregado[] empregado = new Empregado[4];
+
+        empregado[0] = new EmpregadoComissionado(1, "Selmini", 350000, 20);
+        empregado[1] = new EmpregadoHorista(2, "João", 160, 550);
+        empregado[2] = new EmpregadoComissionado(3, "Patrícia", 700000, 23);
+        empregado[3] = new EmpregadoHorista(4, "Maria", 200, 800);
+
+        // for tradicional
+        for(int i = 0; i < empregado.length; i++) {
+            System.out.println(empregado[i].matricula);
+            System.out.println(empregado[i].nome);
+            System.out.println();
+        }
+
+        // for genérico
+        System.out.println("#####################################");
+        for(Empregado e : empregado) {
+            System.out.println(e.matricula);
+            System.out.println(e.nome);
+            System.out.println(e.calcularSalario());
+            System.out.println();
+        }
+
+    }
 }
