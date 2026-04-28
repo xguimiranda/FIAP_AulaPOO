82dd84e472df5cfba068de3fa03f18d1192bf57c
xguimiranda <saguilherme16@gmail.com>
2026-04-28 11:36:50 -0300

    FEAT: tostring class Horista

--- src/aula9_Heranca/EmpregadoHorista.java
+++ src/aula9_Heranca/EmpregadoHorista.java
@@ -13,4 +13,14 @@ public class EmpregadoHorista extends Empregado {
     public double calcularSalario() {
         return totalDeHorasTrabalhadas * valorDaHora;
     }
+
+    @Override
+    public String toString() {
+        return "EmpregadoHorista{" +
+                "totalDeHorasTrabalhadas=" + totalDeHorasTrabalhadas +
+                ", valorDaHora=" + valorDaHora +
+                ", matricula=" + matricula +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
